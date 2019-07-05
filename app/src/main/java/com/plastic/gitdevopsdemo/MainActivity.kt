48c804361e1f9cc28bb45ae5b8962ae29d658a89
karan.maru <karan.maru@plasticmobile.com>
2019-07-05 09:05:38 -0400

    temp commit

--- app/src/main/java/com/plastic/gitdevopsdemo/MainActivity.kt
+++ app/src/main/java/com/plastic/gitdevopsdemo/MainActivity.kt
@@ -8,6 +8,7 @@ import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
 
+    //temp commit
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
