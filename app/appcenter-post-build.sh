b5cdd5319cae600db96f2250923602348da2611d
karan.maru <karan.maru@plasticmobile.com>
2019-07-08 16:00:06 -0400

    update slack notification post script

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -14,41 +14,80 @@
 # Uses UNIX LF EOL format.
 #
 
-# Update your ORG name and APP name
-USR=karan_maru
-APP=GitDevopsDemo
-# This is to get the Build Details so we could pass it as part of the Email Body
-build_url=https://appcenter.ms/users/$USR/apps/$APP/build/branches/$APPCENTER_BRANCH/builds/$APPCENTER_BUILD_ID
-# Address to send email
-TO_ADDRESS="karan.maru@plasticmobile.com"
-# A sample Subject Titlegit rm -r --cached .
-SUBJECT="AppCenter Build"
-# Content of the Email on Build-Success.
-SUCCESS_BODY="Success! Your build completed successfully!\n\n"
-# Content of the Email on Build-Failure.
-FAILURE_BODY="Sorry! Your AppCenter Build failed. Please review the logs and try again.\n\n"
-#If Agent Job Build Status is successful, Send the email, if not send a failure email.
-if [ "$AGENT_JOBSTATUS" == "Succeeded" ];
-then
-	echo "Build Success!"
-	echo -e ${SUCCESS_BODY} ${build_url} | mail -s "${SUBJECT} - Success!" ${TO_ADDRESS}
-	echo "success mail sent"
-else
-	echo "Build Failed!"
-	echo -e ${FAILURE_BODY} ${build_url} | mail -s "${SUBJECT} - Failed!" ${TO_ADDRESS}
-	echo "failure mail sent"
-fi
+## Update your ORG name and APP name
+#USR=karan_maru
+#APP=GitDevopsDemo
+## This is to get the Build Details so we could pass it as part of the Email Body
+#build_url=https://appcenter.ms/users/$USR/apps/$APP/build/branches/$APPCENTER_BRANCH/builds/$APPCENTER_BUILD_ID
+## Address to send email
+#TO_ADDRESS="karan.maru@plasticmobile.com"
+## A sample Subject Titlegit rm -r --cached .
+#SUBJECT="AppCenter Build"
+## Content of the Email on Build-Success.
+#SUCCESS_BODY="Success! Your build completed successfully!\n\n"
+## Content of the Email on Build-Failure.
+#FAILURE_BODY="Sorry! Your AppCenter Build failed. Please review the logs and try again.\n\n"
+##If Agent Job Build Status is successful, Send the email, if not send a failure email.
+#if [ "$AGENT_JOBSTATUS" == "Succeeded" ];
+#then
+#	echo "Build Success!"
+#	echo -e ${SUCCESS_BODY} ${build_url} | mail -s "${SUBJECT} - Success!" ${TO_ADDRESS}
+#	echo "success mail sent"
+#else
+#	echo "Build Failed!"
+#	echo -e ${FAILURE_BODY} ${build_url} | mail -s "${SUBJECT} - Failed!" ${TO_ADDRESS}
+#	echo "failure mail sent"
+#fi
 
 
 # Send a slack notification specifying whether or
 # not a build successfully completed.
 
-webhookUrl=https://hooks.slack.com/services/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
-
 cd $APPCENTER_SOURCE_DIRECTORY
-curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "payload={
-        \"user_name\": \"karan.maru@plasticmobile.com\",
-        \"username\": \"DevopsTesting\",
-        \"text\": \"APP [DevopsDemo] build success and distributed.\",
-        \"icon_emoji\": \":joy_cat:\"
-      }" \ $webhookUrl
+
+ORG=quicktype
+APP=quicktype
+
+ICON=https://pbs.twimg.com/profile_images/881784177422725121/hXRP69QY_200x200.jpg
+
+build_url=https://appcenter.ms/orgs/$ORG/apps/$APP/build/branches/$APPCENTER_BRANCH/builds/$APPCENTER_BUILD_ID
+build_link="<$build_url|$APP $APPCENTER_BRANCH #$APPCENTER_BUILD_ID>"
+
+version() {
+    cat package.json | jq -r .version
+}
+
+slack_notify() {
+#    local message
+#    local "${@}"
+
+    curl -X POST --data-urlencode \
+        "payload={
+            \"channel\": \"#notifications\",
+            \"username\": \"App Center\",
+            \"text\": \"Hello\",
+            \"icon_url\": \"$ICON\" \
+        }" \
+        $SLACK_WEBHOOK
+}
+
+slack_notify_build_passed() {
+    slack_notify message="✓ $build_link built"
+}
+
+slack_notify_build_failed() {
+    slack_notify message="💥 $build_link build failed"
+}
+
+slack_notify_deployed() {
+    slack_notify message="✓ <$build_url|$APP v`version`> released to npm"
+}
+
+slack_notify_homebrew_bump() {
+    slack_notify message="✓ <https://github.com/Homebrew/homebrew-core/pulls|$APP v`version`> bump PR sent to Homebrew"
+}
+
+if [ "$AGENT_JOBSTATUS" != "Succeeded" ]; then
+    slack_notify_build_failed
+    exit 0
+fi
