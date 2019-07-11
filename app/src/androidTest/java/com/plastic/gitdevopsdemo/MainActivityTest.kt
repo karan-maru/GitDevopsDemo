51252eb60dc5ccb1064fe901307ee1516ea416a5
karan.maru <karan.maru@plasticmobile.com>
2019-07-11 09:26:18 -0400

    remove reportHelper error

---
+++ app/src/androidTest/java/com/plastic/gitdevopsdemo/MainActivityTest.kt
@@ -0,0 +1,96 @@
+package com.plastic.gitdevopsdemo
+
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.view.View
+import android.view.ViewGroup
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+//import org.junit.After;
+//import com.microsoft.appcenter.espresso.Factory;
+//import com.microsoft.appcenter.espresso.ReportHelper;
+//import android.R.attr.label
+
+
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+//    @Rule
+//    public var reportHelper = Factory.getReportHelper()
+
+    @Test
+    fun mainActivityTest() {
+//        reportHelper.label("bottonviewTest")
+
+        val appCompatButton = onView(
+            allOf(
+                withId(R.id.button), withText("Click Me!!"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatButton.perform(click())
+
+        val button = onView(
+            allOf(
+                withId(R.id.button),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        button.check(matches(isDisplayed()))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+
+//    @After
+//    fun TearDown() {
+//        reportHelper.label("Stopping App")
+//    }
+}
